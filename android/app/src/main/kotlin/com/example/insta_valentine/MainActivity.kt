ead63fbf0640df89b6a44c9af2cb08d25899f829
Ponlakit Konkrathok <Ponlakit13@hotmail.com>
2022-02-26 23:51:20 +0700

    Homework #10

---
+++ android/app/src/main/kotlin/com/example/insta_valentine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.insta_valentine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/cupertino.dart';
+
+import 'insta.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  const app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // callback method
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        primarySwatch: Colors.purple,
+      ),
+      home: Test(),
+    );
+  }
+}
+
+class Test extends StatelessWidget {
+  Test({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow.shade600,
+      appBar: AppBar(title: Text('',
+          style: TextStyle(fontStyle: FontStyle.italic,
+      body: ListView.builder(
+          itemCount: InstaList.length,
+          itemBuilder: (context, index) =>
+              MyCard(insta: InstaList[index])),
+
+    );
+  }
+}
+class MyCard extends StatefulWidget {
+  final Insta insta;
+
+  const MyCard({
+    Key? key,
+    required this.insta,
+  }) : super(key: key);
+
+  @override
+  _MyCardState createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+  final controller = TextEditingController();
+  var like = false;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5.0,
+      margin: EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //Text(covid.date),
+
+          Image.asset('${widget.insta.img}',fit: BoxFit.cover,),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                /*SizedBox(width: 5.0),*/
+                Expanded(
+                    child: Text(
+                        '${widget.insta.caption}'))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0,left: 16.0,right: 16.0),
+            child: Column(
+              children: [
+                for(int i=0;i<widget.insta.comments.length;i++)
+                  Row(
+                    children: [
+                      SizedBox(height: 23.0,),
+                      Text(
+                        '${widget.insta.comments[i].user}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple.shade200,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      SizedBox(width: 5.0,),
+                      Text(
+                        '${widget.insta.comments[i].comment}',
+                        style: TextStyle(
+
+                        ),
+                      )
+                    ],
+                  ),
+              ],
+            ),
+          ),
+
+
+
+
+        ],
+      ),
+    );
+  }
+}
